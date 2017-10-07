2b1b808d1ba33167d8ea0eea7295f11f418bdf23
dailybp <bpdailey@gmail.com>
2017-10-07 08:01:18 -0400

    First Draft without MomentJS

--- script.js
+++ script.js
@@ -10,3 +10,42 @@ var config = {
 
   var dataRef = firebase.database();
   console.log(dataRef);
+
+  $("#submit-btn").on("click", function(event){
+    event.preventDefault();
+
+    var trainName = $("#train-name").val().trim();
+    var trainDestination = $("#train-destination").val().trim();
+    var firstTrainTime = $("#first-train-time").val().trim();
+    var trainFrequency = $("#frequency").val().trim();
+
+    var newTrain = {
+      name: trainName,
+      destination: trainDestination;
+      time: firstTrainTime;
+      frequency: trainFrequency;
+    };
+
+    dataRef.push(newTrain);
+    alert("Your Train Has Been Added!");
+
+    $("#train-name").val("");
+    $("#train-destination").val("");
+    $("#first-train-time").val("");
+    $("#frequency").val("");
+  });
+
+  database.ref().on("child_added", function(childSnapshot, prevChildKey){
+    console.log(childSnapshot.val());
+
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().destination;
+    var firstTrainTime = childSnapshot.val().time;
+    var trainFrequency = childSnapshot.val().frequency;
+
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(firstTrainTime);
+    console.log(trainFrequency);
+
+  });
